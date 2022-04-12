a2243f05c975475672de00b5fd342259b28274d7
Andreas Lillje <al225wg@student.lnu.se>
2022-04-12 12:49:18 +0200

    Add auth-reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import { errorReducer } from './error-reducer'
+import { authReducer } from './auth-reducer'
+
+export default combineReducers({
+    error: errorReducer,
+    auth: authReducer
+})
