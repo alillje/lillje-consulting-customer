f7efd75ba9db042a8210a65e108135f926d73e63
Andreas Lillje <al225wg@student.lnu.se>
2022-04-25 15:04:27 +0200

    Feature/layout transactions submenu

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { useState } from "react";
 import "./sidebar.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 
 import Drawer from "@mui/material/Drawer";
@@ -10,12 +10,16 @@ import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+
 // Icons
 import AddBoxSharpIcon from "@mui/icons-material/AddBoxSharp";
 import ViewListSharpIcon from "@mui/icons-material/ViewListSharp";
 import LogoutSharpIcon from "@mui/icons-material/LogoutSharp";
-import ManageSearchSharpIcon from '@mui/icons-material/ManageSearchSharp';
-
+import ManageSearchSharpIcon from "@mui/icons-material/ManageSearchSharp";
+import KeyboardDoubleArrowRightSharpIcon from '@mui/icons-material/KeyboardDoubleArrowRightSharp';
 import { useSelector } from "react-redux";
 import { logoutHandler } from "../../services/logout-service";
 
@@ -32,8 +36,10 @@ const Sidebar = () => {
     logoutHandler(user);
   };
 
-  const goTo = (event) => {
-    console.log(event.target.value);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
   };
 
   const drawerWidth = 250;
@@ -52,19 +58,96 @@ const Sidebar = () => {
       <Box sx={{ overflow: "auto" }}>
         <Toolbar />
         <List>
-        <ListItem button component={Link} to="/transactions/register">
+          <ListItem button component={Link} to="/transactions/register">
             <ListItemIcon>
               <AddBoxSharpIcon />
             </ListItemIcon>
             <ListItemText primary="Ny transaktion" />
           </ListItem>
 
-          <ListItem button component={Link} to="/transactions">
+          <ListItem onClick={handleClick}>
             <ListItemIcon>
               <ViewListSharpIcon />
             </ListItemIcon>
             <ListItemText primary="Transaktioner" />
+            {open ? <ExpandLess /> : <ExpandMore />}
           </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem
+                button
+                component={Link}
+                to="/transactions"
+                sx={{ pl: 4 }}
+              >
+                <ListItemIcon>
+                <KeyboardDoubleArrowRightSharpIcon fontSize="xs" />
+                </ListItemIcon>
+                <ListItemText primary="Alla transaktioner" />
+              </ListItem>
+              <ListItem
+                button
+                component={Link}
+                to="/transactions"
+                sx={{ pl: 4 }}
+              >
+                <ListItemIcon>
+                <KeyboardDoubleArrowRightSharpIcon fontSize="xs" />
+
+                </ListItemIcon>
+                <ListItemText primary="Hanterade transaktioner" />
+              </ListItem>
+              <ListItem
+                button
+                component={Link}
+                to="/transactions"
+                sx={{ pl: 4 }}
+              >
+                <ListItemIcon>
+                <KeyboardDoubleArrowRightSharpIcon fontSize="xs" />
+
+                </ListItemIcon>
+                <ListItemText primary="Ohanterade transaktioner" />
+              </ListItem>
+              <ListItem
+                button
+                component={Link}
+                to="/transactions"
+                sx={{ pl: 4 }}
+              >
+                <ListItemIcon>
+                <KeyboardDoubleArrowRightSharpIcon fontSize="xs" />
+
+                </ListItemIcon>
+                <ListItemText primary="Leverantörsfakturor" />
+              </ListItem>
+              <ListItem
+                button
+                component={Link}
+                to="/transactions"
+                sx={{ pl: 4 }}
+              >
+                <ListItemIcon>
+                <KeyboardDoubleArrowRightSharpIcon fontSize="xs" />
+
+                </ListItemIcon>
+                <ListItemText primary="Kundfakturor" />
+              </ListItem>
+              <ListItem
+                button
+                component={Link}
+                to="/transactions"
+                sx={{ pl: 4 }}
+              >
+                <ListItemIcon>
+                <KeyboardDoubleArrowRightSharpIcon fontSize="xs" />
+
+                </ListItemIcon>
+                <ListItemText primary="Utlägg" />
+              </ListItem>
+            </List>
+          </Collapse>
+
           <ListItem button component={Link} to="/transactions">
             <ListItemIcon>
               <ManageSearchSharpIcon />
